0bdb07018a45422ae620f50b369ee0ab5f1fbcd3
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-15 00:17:53 +0200

    Create color.go

---
+++ utils/color.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+type Text struct {
+	Text           string
+	ColorAttribute color.Attribute
+	Style          []color.Attribute
+}
+
+func DisableColorOutput() {
+	color.NoColor = true
+}
+
+func PrintColorText(txt *Text) {
+	if color.NoColor {
+		fmt.Println(txt.Text)
+		return
+	}
+	attributes := []color.Attribute{txt.ColorAttribute}
+	attributes = append(attributes, txt.Style...)
+
+	outLine := color.New(attributes...)
+	outLine.Println(txt.Text)
+}
