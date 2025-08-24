10ecdf8b81fb65786b3fd6567842877adcc27a4c
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-24 10:47:12 +0200

    Update constant.go
    Add way for file

--- data/constant.go
+++ data/constant.go
@@ -1,5 +1,7 @@
 package data
 
 const (
-	VERSION string = "0.0.1"
+	VERSION               string = "0.0.1"
+	WAY_DATA_JSON         string = "data/data.json"
+	WAY_POPULAR_HASH_JSON string = "data/popularHash.json"
 )
