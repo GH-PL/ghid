48e9cc36e555a095d4e307dfbdae4928af3f5279
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-24 11:02:19 +0200

    Update constant.go

--- data/constant.go
+++ data/constant.go
@@ -1,7 +1,7 @@
 package data
 
 const (
-	VERSION               string = "0.0.1"
+	VERSION               string = "GHID version 0.0.1"
 	WAY_DATA_JSON         string = "data/data.json"
 	WAY_POPULAR_HASH_JSON string = "data/popularHash.json"
 )
