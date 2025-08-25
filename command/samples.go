70b74cf7f6e3f104177a47deb4e965c7edef465b
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-25 19:03:30 +0200

    Update samples.go

--- command/samples.go
+++ command/samples.go
@@ -1,11 +1,11 @@
 package command
 
 import (
-	"fmt"
 	"strings"
 
-	"github.com/GH-PL/ghid/flags"
-	"github.com/GH-PL/ghid/utils"
+	"ghid/err"
+	"ghid/flags"
+	"ghid/utils"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
@@ -15,16 +15,11 @@ var SamplesCmd = &cobra.Command{
 	Use:   "samples [NAME HASH]",
 	Short: "Display hash samples for the given type",
 	Long:  "Display hash samples for the given type",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
-			utils.PrintColorText(&utils.Text{
-				Text:           "Error: missing hashType argument",
-				ColorAttribute: color.FgRed,
-				Style:          []color.Attribute{color.Bold},
-			})
-			return
+			return err.ErrEmptyArgument
 		}
-		samples(args[0])
+		return samples(args[0])
 	},
 }
 
@@ -32,41 +27,30 @@ func init() {
 	flags.AddBoolFlags(SamplesCmd, flags.BoolFlags)
 }
 
-func samples(str string) {
+func samples(str string) error {
 	hash := utils.ParseJson()
 	if color.NoColor {
 		utils.DisableColorOutput()
 	}
-	found := false
 
 	for _, hashValue := range hash {
 		for _, mode := range hashValue.Modes {
-			if strings.EqualFold(str, mode.Name) {
-				if len(mode.Samples) == 0 {
-					utils.PrintColorText(&utils.Text{
-						Text:           "No samples available for this Hash",
-						ColorAttribute: color.FgRed,
-						Style:          []color.Attribute{color.Bold},
-					})
-				} else {
-					for _, samplesValue := range mode.Samples {
-						utils.PrintColorText(&utils.Text{
-							Text:           samplesValue,
-							ColorAttribute: color.FgBlue,
-							Style:          []color.Attribute{color.Bold},
-						})
-					}
-				}
 
-				found = true
+			if !strings.EqualFold(str, mode.Name) {
+				continue
 			}
+			if len(mode.Samples) == 0 {
+				return err.ErrNoSamplesFound
+			}
+			for _, samplesValue := range mode.Samples {
+				utils.PrintColorText(&utils.Text{
+					Text:           samplesValue,
+					ColorAttribute: color.FgBlue,
+					Style:          []color.Attribute{color.Bold},
+				})
+			}
+			return nil
 		}
 	}
-	if !found {
-		utils.PrintColorText(&utils.Text{
-			Text:           fmt.Sprintf("Not found %s", str),
-			ColorAttribute: color.FgRed,
-			Style:          []color.Attribute{color.Bold},
-		})
-	}
+	return err.ErrNoSamplesFound
 }
