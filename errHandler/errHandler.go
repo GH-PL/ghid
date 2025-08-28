0addd55ff71a9481cdf3f5ab494be5de4aa0addf
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-28 15:35:02 +0200

    Update errHandler.go

--- errHandler/errHandler.go
+++ errHandler/errHandler.go
@@ -7,17 +7,7 @@ import (
 	"log"
 	"os"
 	"syscall"
-
-	"github.com/fatih/color"
 )
-
-func Show(err error) {
-	output.PrintColorText(&output.Text{
-		Text:           err.Error(),
-		ColorAttribute: color.FgRed,
-		Style:          []color.Attribute{color.Bold},
-	})
-}
 
 type IsERROR struct {
 	Err error
@@ -26,16 +16,16 @@ type IsERROR struct {
 
 func IsError(err *IsERROR) {
 	if os.IsPermission(err.Err) {
-		Show(fmt.Errorf("Permission denied for file: %s", err.Msg))
+		output.PrintError(fmt.Errorf("Permission denied for file: %s", err.Msg))
 	}
 	if os.IsNotExist(err.Err) {
-		Show(fmt.Errorf("The file does not exist: %s", err.Msg))
+		output.PrintError(fmt.Errorf("The file does not exist: %s", err.Msg))
 	}
 	if errors.Is(err.Err, syscall.EISDIR) {
-		Show(fmt.Errorf("Expected file but found directory: %s", err.Msg))
+		output.PrintError(fmt.Errorf("Expected file but found directory: %s", err.Msg))
 	}
 	if errors.Is(err.Err, syscall.EINVAL) {
-		Show(fmt.Errorf("Invalid type"))
+		output.PrintError(fmt.Errorf("Invalid type"))
 	}
 	log.Fatal("Error. App not work")
 }
