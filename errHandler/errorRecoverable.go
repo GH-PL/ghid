3c5f68d89137d9e126aacba7e73069f02959a15c
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-28 16:57:23 +0200

    Update errorRecoverable.go

--- errHandler/errorRecoverable.go
+++ errHandler/errorRecoverable.go
@@ -7,4 +7,6 @@ var (
 	ErrEmptyArgument   = errors.New("Error: missing required argument <hash name>")
 	ErrNotFoundHash    = errors.New("Not found type for this Hash")
 	ErrNotExampleFound = errors.New("No examples found")
+	ErrNotFoundName    = errors.New("Not found this Hash for name")
 )
+
