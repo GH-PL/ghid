cff78bc2b4802ab6ffa451ff34a48c9330e67594
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-28 16:37:14 +0200

    Create fileTypeError.go

---
+++ errHandler/fileTypeError.go
@@ -0,0 +1,28 @@
+package errHandler
+
+import (
+	"fmt"
+	"ghid/output"
+)
+
+type FileError struct {
+	Operation string
+	Path      string
+	Err       error
+}
+
+func ErrorFile(operation string, path string, err error) {
+	txtErr := &FileError{
+		Operation: operation,
+		Path:      path,
+		Err:       err,
+	}
+	output.PrintError(txtErr.Error())
+}
+
+func (e *FileError) Error() error {
+	return fmt.Errorf("error during: %s of file: %s -- %v", e.Operation, e.Path, e.Err)
+}
+func (e *FileError) Unwrap() error {
+	return e.Err
+}
