213263f43cbab088505c0911d22503b8187957ab
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-28 13:23:25 +0200

    Update errorRecoverable.go

--- errHandler/errorRecoverable.go
+++ errHandler/errorRecoverable.go
@@ -4,12 +4,5 @@ import "errors"
 
 // Error
 var (
-	ErrEmptyArgument   = errors.New("Error: missing required argument <hash name>")
-	ErrMissingArgument = errors.New("Missing required argument")
-	ErrInvalidFormat   = errors.New("Invalid format")
-)
-
-// Not found
-var (
-	ErrNoSamplesFound = errors.New("No examples found")
+	ErrEmptyArgument = errors.New("Error: missing required argument <hash name>")
 )
