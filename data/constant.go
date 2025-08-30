9ce86c32cf1fcc3079483f0d9fe8df8b960e3640
GH-PL <serhii.kobrin.pl@gmail.com>
2025-08-31 01:36:19 +0200

    Update constant.go

--- data/constant.go
+++ data/constant.go
@@ -4,5 +4,5 @@ const (
 	VERSION              string = "GHID version 0.0.1"
 	WAY_DATA_JSON        string = "data/data.json"
 	WAY_POPULAR_HASH_CSV string = "data/popularHash.csv"
-	DEFAULT_DECRYPT_FILE string = "D:/.ghid/decrypt.txt"
+	DEFAULT_DECRYPT_FILE string = "decrypt.txt"
 )
